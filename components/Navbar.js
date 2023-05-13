4854a3a8963f3e310b4bc949d78d0ba6b861c424
Techbite <bonfacemwema7@gmail.com>
2023-05-13 11:24:09 +0300

    Next-auth client side authentication

---
+++ components/Navbar.js
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import { useSession, signIn, signOut } from "next-auth/react"
+
+
+export default function Navbar() {
+ const {data: session} = useSession()
+
+ console.log(session)
+
+  return (
+    <nav className="header">
+      <h1 className="logo">
+        <a href="#">NextAuth</a>
+      </h1>
+      <ul className={`main-nav`}>
+        <li>
+          <Link href="/">
+            <h2>Home</h2>
+          </Link>
+        </li>
+        <li>
+          <Link href="/dashboard"> 
+            <h2>Dashboard</h2>
+          </Link>
+        </li>
+
+        <li>
+          <Link href="/blog">
+            <h2>Blog</h2>
+          </Link>
+        </li>
+        {!session && (
+            <li>
+            <Link href="/api/auth/signin">
+            <button onClick={() => signIn()}>Sign in</button>
+            </Link>
+          </li>
+        )}
+        {session && (
+            <li>
+            <Link href="/api/auth/signout">
+            <button onClick={() => signOut()}>Sign out</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}
+
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
 import '@/styles/globals.css'
+import Navbar from '@/components/Navbar'
+import '../components/Navbar.css'
+import { SessionProvider } from "next-auth/react"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// export default function App({ Component, pageProps }) {
+//   return(
+//     <Provider session={pageProps.session}>
+//     <SessionProvider session={pageProps.session}>
+//       <Navbar/>
+//       <Component {...pageProps} />
+//     </SessionProvider>
+//   </Provider>
+//   )
+  
+// }
+
+export default function App({ Component, pageProps: { session, ...pageProps }, }) {
+  return (
+    <SessionProvider session={session}>
+      <Navbar/>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
