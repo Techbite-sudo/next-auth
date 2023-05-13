b1e8addcb3cb28179a95d6364d726b74942ed8a1
Techbite <bonfacemwema7@gmail.com>
2023-05-13 13:09:19 +0300

    server side authentication

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,19 @@
-export default function Blog() {
+import { getSession } from "next-auth/react"
+
+export default function Blog({data}) {
     return (
       <div>
-          <h1>Blog page</h1>
+          <h1>Blog page - {data}</h1>
       </div>
     )
   }
   
+  export async function getServerSideProps(context){
+    const session = await getSession(context)
+    return{
+        props: {
+            data: session? 'List of 100 personalized blogs':'List of Free blogs',
+            session,
+        }
+    }
+}
