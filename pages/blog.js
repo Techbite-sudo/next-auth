fe009dc4fa8075b2c69a6a7b12fec5658e074065
Techbite <bonfacemwema7@gmail.com>
2023-05-13 13:19:03 +0300

    securing pages server side

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,8 @@
-import { getSession } from "next-auth/react"
+import { getSession, useSession } from "next-auth/react"
 
 export default function Blog({data}) {
+    const { data: session } = useSession()
+    console.log({session})
     return (
       <div>
           <h1>Blog page - {data}</h1>
@@ -10,6 +12,16 @@ export default function Blog({data}) {
   
   export async function getServerSideProps(context){
     const session = await getSession(context)
+
+    if (!session) {
+        return{
+            redirect: {
+                destination: `/api/auth/signin?callbackUrl=https://localhost:3000/blog`,
+                permanent: false,
+            }
+        }
+    }
+    
     return{
         props: {
             data: session? 'List of 100 personalized blogs':'List of Free blogs',
